f8eeb635153c49b671cfae27bcfab5eaa094f312
Shadow-Walker96 <86225985+Shadow-Walker96@users.noreply.github.com>
2023-10-01 13:29:56 +0000

    complete packages

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,6 @@ import Link from "next/link"
 
 export default function Header() {
     return (
-        // <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
 		<nav className="p-5 border-b-2 flex flex-row justify-between items-center">
             <h1 className="py-4 px-4 font-bold text-3xl">NFT Marketplace</h1>
             <div>
