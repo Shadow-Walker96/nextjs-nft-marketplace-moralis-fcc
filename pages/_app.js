4b01aa16f0e2b5aa495bd73cc28a2340796d46d2
Shadow-Walker96 <86225985+Shadow-Walker96@users.noreply.github.com>
2023-10-01 23:14:31 +0000

    another commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,41 @@
+// 25:19:37 ---------> Connecting Moralis To Our Local Hardhat Node
+
+import "../styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import Header from "../components/Header"
+import Head from "next/head"
+
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
+
+function MyApp({ Component, pageProps }) {
+    return (
+        <div>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="NFT Marketplace" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+                <Header />
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </div>
+    )
+}
+
+export default MyApp
+
+
+
+
+
+
+
+
+
+
+/*
 import "../styles/globals.css"
 import { MoralisProvider } from "react-moralis"
 import Header from "../components/Header"
@@ -20,3 +58,4 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+*/
